ff24e494362bfaf57dbc32f897d7d7a1c7d2e405
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2014-02-26 09:35:10 +0100

    added field imei to model (OfficeInfo member)

--- src/ralph_assets/models_assets.py
+++ src/ralph_assets/models_assets.py
@@ -499,8 +499,8 @@ class OfficeInfo(TimeTrackable, SavingUser, SoftDeletable):
     date_of_last_inventory = models.DateField(
         null=True, blank=True)
     last_logged_user = models.CharField(max_length=100, null=True, blank=True)
-    imei = models.URLField(
-        max_length=18, null=True, blank=True, unique=False
+    imei = models.CharField(
+        max_length=18, null=True, blank=True, unique=False, verbose_name='IMEI'
     )
 
     def __unicode__(self):
